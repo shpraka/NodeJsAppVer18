eff675efce9d4a826f2eebe36196aee5e0507d07
Shashi Prakash <shpraka@microsoft.com>
2022-06-16 16:35:42 +0530

    body change

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', body: "tetts" });
+  res.render('index', { title: 'Express', body: "Test Body" });
 });
 
 module.exports = router;
