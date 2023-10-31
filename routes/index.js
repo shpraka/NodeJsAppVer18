364ce5b0d9c9b1fd8891e4f8bad87ff7df7366d2
shpraka <46188819+shpraka@users.noreply.github.com>
2023-10-31 12:31:55 +0530

    2nd commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'NodeJS', body: "31st October 1" });
+  res.render('index', { title: 'NodeJS', body: "31st October 2" });
 });
 
 module.exports = router;
