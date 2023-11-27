2074359c559cfb62d8a837362c75a51a8b679845
shpraka <46188819+shpraka@users.noreply.github.com>
2023-11-27 14:29:40 +0530

    test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'NodeJS', body: "27th November Clean xml" });
+  res.render('index', { title: 'NodeJS', body: "27th November Clean xml1" });
 });
 
 module.exports = router;
