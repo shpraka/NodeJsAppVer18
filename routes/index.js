29fb69d77e30d307554527ed77ed89caaff59112
shpraka <46188819+shpraka@users.noreply.github.com>
2023-12-04 14:56:55 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'NodeJS', body: "4th Dec 2:14 PM" });
+  res.render('index', { title: 'NodeJS', body: "4th Dec 2:56 PM" });
 });
 
 module.exports = router;
